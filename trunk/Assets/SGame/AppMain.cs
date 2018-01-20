ae7008113ea285663df6be2528fcb7abab3980b8
1832854282 <1832854282@qq.com>
2018-01-20 19:01:20 +0800

    UI模块第一版

---
+++ trunk/Assets/SGame/AppMain.cs
@@ -0,0 +1,53 @@
+﻿using SGF;
+using UnityEngine;
+using SGF.UI.Framework;
+using SGF.Module.Framework;
+using SGF.Network;
+using SGame;
+
+public class AppMain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+		Debuger.Log (Debuger.LogFileDir);
+
+       // AppConfig.Init();
+
+	    InitServices();
+	    InitBusiness();
+
+        ModuleManager.Instance.ShowModule(ModuleDef.LoginModule);
+    }
+
+    private void InitServices()
+    {
+        ModuleManager.Instance.Init("SGame.Module");
+
+		NetworkManager.Instance.Init ();
+
+        UIManager.Instance.Init("UI/");
+        UIManager.MainPage = UIDef.UIHomePage;
+        UIManager.MainScene = "Main";
+
+       // UserManager.Instance.Init();
+
+      //  GameManager.Instance.Init();
+    }
+
+
+    private void InitBusiness()
+    {
+        ModuleManager.Instance.CreateModule(ModuleDef.LoginModule);
+        ModuleManager.Instance.CreateModule(ModuleDef.HomeModule);
+        ModuleManager.Instance.CreateModule(ModuleDef.PVEModule);
+        ModuleManager.Instance.CreateModule(ModuleDef.PVPModule);
+        ModuleManager.Instance.CreateModule(ModuleDef.HostModule);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+}
---
+++ trunk/Assets/SGame/UI/Home/UIHomePage.cs
@@ -0,0 +1,94 @@
+﻿using SGF.Module.Framework;
+using SGF.UI.Framework;
+
+using UnityEngine.UI;
+
+namespace SGame
+{
+    public class UIHomePage:UIPage
+    {
+        public Text txtUserInfo;
+
+        protected override void OnOpen(object arg = null)
+        {
+            base.OnOpen(arg);
+            UpdateUserInfo();
+        }
+
+
+        private void UpdateUserInfo()
+        {
+           // UserData ud = UserManager.Instance.MainUserData;
+            txtUserInfo.text = "哈哈 lv.5";//ud.name + "(Lv."+ud.level+")";
+        }
+
+
+        public void OnBtnUserInfo()
+        {
+            UIAPI.ShowMsgBox("重新登录", "是否重新登录？", "确定|取消", o =>
+            {
+                if ((int) o == 0)
+                {
+                   // HomeModule module = ModuleManager.Instance.GetModule(ModuleDef.HomeModule) as HomeModule;
+                   // module.TryReLogin();
+                }
+            });
+        }
+
+
+        private void OpenModule(string name, object arg = null)
+        {
+            /* 
+            var module = ModuleManager.Instance.GetModule("HomeModule") as HomeModule;
+            if (module != null)
+            {
+                module.OpenModule(name, arg);
+            }
+            */
+        }
+
+
+        public void OnBtnSetting()
+        {
+            OpenModule(ModuleDef.SettingModule);
+        }
+
+        public void OnBtnDailyCheckIn()
+        {
+            OpenModule(ModuleDef.DailyCheckInModule);
+        }
+
+        public void OnBtnActivity()
+        {
+            OpenModule(ModuleDef.ActivityModule);
+        }
+
+        public void OnBtnBuyCoin()
+        {
+            OpenModule(ModuleDef.ShopModule, "BuyCoin");
+        }
+
+        public void OnBtnFreeCoin()
+        {
+            OpenModule(ModuleDef.ShareModule);
+        }
+
+
+        public void OnBtnEndlessPVE()
+        {
+			//OpenModule(ModuleDef.PVEModule, (int)GameMode.EndlessPVE);
+        }
+
+        public void OnBtnTimelimitPVE()
+        {
+			//OpenModule(ModuleDef.PVEModule, (int)GameMode.TimelimitPVE);
+        }
+
+        public void OnBtnPVP()
+        {
+            //OpenModule(ModuleDef.PVPModule, (int)GameMode.EndlessPVP);
+        }
+
+
+    }
+}
